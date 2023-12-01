e79e3939fc3912f420f05d35c29a2e59a30167fd
Chris McComb <ccmcc2012@gmail.com>
2023-12-01 12:40:17 -0500

    Big initial commit

---
+++ update.py
@@ -0,0 +1,78 @@
+import json  # for saving and parsing json files
+import os  # for searching for json files
+
+import matplotlib.colors  # for getting pretty colors
+import matplotlib.pyplot  # for converting rgb to hex
+import numpy  # for generic operations
+import pandas  # dumping json into csv
+import plotly.express  # plotly
+import sentence_transformers  # embeddings
+import sklearn.decomposition  # orient tsne
+import sklearn.manifold  # make a tsne
+from numpy.typing import NDArray
+import urllib.request
+import json
+
+# Facult lists
+list_of_maps: list[str] = ["https://raw.githubusercontent.com/cmccomb/map-of-mech/",
+                           "https://raw.githubusercontent.com/cmccomb/map-of-civil/",]
+
+FACULTY_EXTENSION: str = "main/faculty.csv"
+JSON_EXTENSION: str = "main/data/"
+
+all_names = []
+
+# Dump all the json files into a single dataframe
+all_the_data: pandas.DataFrame = pandas.DataFrame()
+for map in list_of_maps:
+    names = pandas.read_csv(map + FACULTY_EXTENSION)['name'].to_list()
+    ids = pandas.read_csv(map + FACULTY_EXTENSION)['id'].replace(numpy.nan, None).to_list()
+    for (name, id) in zip(names, ids):
+        print(name, id)
+        if id:
+            encoded_name = name.replace(" ", "%20")
+            json_url = map + JSON_EXTENSION + encoded_name + ".json"
+            all_names.append(name)
+
+            with urllib.request.urlopen(json_url) as url:
+                json_dict: dict = json.load(url)
+                json_as_df: pandas.DataFrame = pandas.DataFrame.from_dict(json_dict)
+                json_as_df['faculty'] = name
+                all_the_data: pandas.DataFrame = pandas.concat([all_the_data, json_as_df], axis=0)
+
+# Re-index the dataframe because it appears to eb necessary
+all_the_data.reset_index(inplace=True)
+
+# Embed titles from publications
+model = sentence_transformers.SentenceTransformer('all-mpnet-base-v2')
+embeddings: numpy.ndarray = model.encode(all_the_data['title'].to_list(), show_progress_bar=True)
+
+# Boil down teh data into a 2D plot
+tsne_embeddings: NDArray = sklearn.manifold.TSNE(n_components=2, random_state=42).fit_transform(embeddings)
+pca_embeddings: NDArray = sklearn.decomposition.PCA(n_components=2, random_state=42).fit_transform(tsne_embeddings)
+all_the_data['x'] = pca_embeddings[:, 0]
+all_the_data['y'] = pca_embeddings[:, 1]
+
+# Make some pretty colors! 
+colors: list[str] = [matplotlib.colors.to_hex(x) for x in
+    matplotlib.pyplot.cm.gist_rainbow(numpy.linspace(0, 1, len(all_names)))]
+
+# Plot the embeddings
+fig = plotly.express.scatter(all_the_data, x="x", y="y", hover_data=["title"], color="faculty", symbol="faculty",
+                             color_discrete_sequence=colors)
+
+# Make sure the axes are appropriately scaled
+fig.update_xaxes(visible=False, autorange=False,
+    range=[numpy.min(pca_embeddings[:, 0]) * 1.05, numpy.max(pca_embeddings[:, 0]) * 1.05])
+
+fig.update_yaxes(visible=False, scaleanchor="x", scaleratio=1,
+    range=[numpy.min(pca_embeddings[:, 1]) * 1.05, numpy.max(pca_embeddings[:, 1]) * 1.05])
+
+# Reset the layout
+fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), legend=dict(y=0.5), plot_bgcolor="#191C1F", )
+
+# Remove the logo
+fig.show(config=dict(displaylogo=False))
+
+# Save the file
+fig.write_html("index.html")
