c4483ec69f5a39bb884f07c5c2790da690634bbf
Chris McComb <ccmcc2012@gmail.com>
2024-05-08 17:33:12 -0400

    Cleanup

--- update_embeddings.py
+++ update_embeddings.py
@@ -1,31 +1,51 @@
 import json  # for saving and parsing json files
-import os  # for searching for json files
 
-import matplotlib.colors  # for getting pretty colors
-import matplotlib.pyplot  # for converting rgb to hex
 import numpy  # for generic operations
 import pandas  # dumping json into csv
-import plotly.express  # plotly
 import sentence_transformers  # embeddings
 import sklearn.decomposition  # orient tsne
 import sklearn.manifold  # make a tsne
 from numpy.typing import NDArray
 import urllib.request
-import json
 
-# Facult lists
+
+# Faculty lists
 list_of_maps: list[tuple[str, str]] = [
-    ("Mechanical Engineering", "https://raw.githubusercontent.com/cmccomb/map-of-mech/"),
-    ("Civil & Environmental Engineering", "https://raw.githubusercontent.com/cmccomb/map-of-civil/"),
-    ("Electrical & Computer Engineering", "https://raw.githubusercontent.com/cmccomb/map-of-ece/"),
-    ("Materials Science & Engineering", "https://raw.githubusercontent.com/cmccomb/map-of-mse/"),
+    (
+        "Mechanical Engineering",
+        "https://raw.githubusercontent.com/cmccomb/map-of-mech/",
+    ),
+    (
+        "Civil & Environmental Engineering",
+        "https://raw.githubusercontent.com/cmccomb/map-of-civil/",
+    ),
+    (
+        "Electrical & Computer Engineering",
+        "https://raw.githubusercontent.com/cmccomb/map-of-ece/",
+    ),
+    (
+        "Materials Science & Engineering",
+        "https://raw.githubusercontent.com/cmccomb/map-of-mse/",
+    ),
     ("Chemical Engineering", "https://raw.githubusercontent.com/cmccomb/map-of-cheme/"),
-    ("Engineering & Public Policy", "https://raw.githubusercontent.com/cmccomb/map-of-epp/"),
+    (
+        "Engineering & Public Policy",
+        "https://raw.githubusercontent.com/cmccomb/map-of-epp/",
+    ),
     ("Biomedical Engineering", "https://raw.githubusercontent.com/cmccomb/map-of-bme/"),
     ("CMU-Africa", "https://raw.githubusercontent.com/cmccomb/map-of-cmu-africa/"),
-    ("CMU Silicon Valley", "https://raw.githubusercontent.com/cmccomb/map-of-cmu-silicon-valley/"),
-    ("Information Networking Institute", "https://raw.githubusercontent.com/cmccomb/map-of-ini/"),
-    ("Integrated Innovation Institute", "https://raw.githubusercontent.com/cmccomb/map-of-ini/"),
+    (
+        "CMU Silicon Valley",
+        "https://raw.githubusercontent.com/cmccomb/map-of-cmu-silicon-valley/",
+    ),
+    (
+        "Information Networking Institute",
+        "https://raw.githubusercontent.com/cmccomb/map-of-ini/",
+    ),
+    (
+        "Integrated Innovation Institute",
+        "https://raw.githubusercontent.com/cmccomb/map-of-ini/",
+    ),
 ]
 
 FACULTY_EXTENSION: str = "main/faculty.csv"
@@ -35,10 +55,14 @@ all_names = []
 
 # Dump all the json files into a single dataframe
 all_the_data: pandas.DataFrame = pandas.DataFrame()
-for map in list_of_maps:    
-    names = pandas.read_csv(map[1] + FACULTY_EXTENSION)['name'].to_list()
-    ids = pandas.read_csv(map[1] + FACULTY_EXTENSION)['id'].replace(numpy.nan, None).to_list()
-    for (name, id) in zip(names, ids):
+for map in list_of_maps:
+    names = pandas.read_csv(map[1] + FACULTY_EXTENSION)["name"].to_list()
+    ids = (
+        pandas.read_csv(map[1] + FACULTY_EXTENSION)["id"]
+        .replace(numpy.nan, None)
+        .to_list()
+    )
+    for name, id in zip(names, ids):
         print(name, id)
         if id:
             encoded_name = name.replace(" ", "%20")
@@ -48,21 +72,29 @@ for map in list_of_maps:
             with urllib.request.urlopen(json_url) as url:
                 json_dict: dict = json.load(url)
                 json_as_df: pandas.DataFrame = pandas.DataFrame.from_dict(json_dict)
-                json_as_df['faculty'] = name
-                json_as_df['department'] = map[0]
-                all_the_data: pandas.DataFrame = pandas.concat([all_the_data, json_as_df], axis=0)
+                json_as_df["faculty"] = name
+                json_as_df["department"] = map[0]
+                all_the_data: pandas.DataFrame = pandas.concat(
+                    [all_the_data, json_as_df], axis=0
+                )
 
 # Re-index the dataframe because it appears to eb necessary
 all_the_data.reset_index(inplace=True)
 
 # Embed titles from publications
-model = sentence_transformers.SentenceTransformer('all-mpnet-base-v2')
-embeddings: numpy.ndarray = model.encode(all_the_data['title'].to_list(), show_progress_bar=True)
+model = sentence_transformers.SentenceTransformer("all-mpnet-base-v2")
+embeddings: numpy.ndarray = model.encode(
+    all_the_data["title"].to_list(), show_progress_bar=True
+)
 
 # Boil down teh data into a 2D plot
-tsne_embeddings: NDArray = sklearn.manifold.TSNE(n_components=2, random_state=42, verbose=True).fit_transform(embeddings)
-pca_embeddings: NDArray = sklearn.decomposition.PCA(n_components=2, random_state=42).fit_transform(tsne_embeddings)
-all_the_data['x'] = pca_embeddings[:, 0]
-all_the_data['y'] = pca_embeddings[:, 1]
+tsne_embeddings: NDArray = sklearn.manifold.TSNE(
+    n_components=2, random_state=42, verbose=True
+).fit_transform(embeddings)
+pca_embeddings: NDArray = sklearn.decomposition.PCA(
+    n_components=2, random_state=42
+).fit_transform(tsne_embeddings)
+all_the_data["x"] = pca_embeddings[:, 0]
+all_the_data["y"] = pca_embeddings[:, 1]
 
-all_the_data.to_csv('data.csv', index=False)
+all_the_data.to_csv("data.csv", index=False)
--- update_visualization.py
+++ update_visualization.py
@@ -1,9 +1,6 @@
-import matplotlib.colors  # for getting pretty colors
-import matplotlib.pyplot  # for converting rgb to hex
 import numpy  # for generic operations
 import pandas  # dumping json into csv
-import plotly.express  # plotly
-
+import plotly.express  # plotly the things
 
 # Load the data
 all_the_data = pandas.read_csv("data.csv")
@@ -15,18 +12,33 @@ all_the_data.sort_values(["department", "faculty"], inplace=True)
 # Plot the embeddings
 fig = plotly.express.scatter(
     all_the_data,
-    x="x", y="y", hover_data=["title", "faculty"],
+    x="x",
+    y="y",
+    hover_data=["title", "faculty"],
     color="department",
-    symbol_sequence=["circle"]
+    symbol_sequence=["circle"],
 )
 
 # Make sure the axes are appropriately scaled
-fig.update_xaxes(visible=False, autorange=False, range=[numpy.min(x_data) * 1.05, numpy.max(x_data) * 1.05])
+fig.update_xaxes(
+    visible=False,
+    autorange=False,
+    range=[numpy.min(x_data) * 1.05, numpy.max(x_data) * 1.05],
+)
 
-fig.update_yaxes(visible=False, scaleanchor="x", scaleratio=1, range=[numpy.min(y_data) * 1.05, numpy.max(y_data) * 1.05])
+fig.update_yaxes(
+    visible=False,
+    scaleanchor="x",
+    scaleratio=1,
+    range=[numpy.min(y_data) * 1.05, numpy.max(y_data) * 1.05],
+)
 
 # Reset the layout
-fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), legend=dict(y=0.5, itemsizing='constant'), plot_bgcolor="#191C1F", )
+fig.update_layout(
+    margin=dict(l=0, r=0, t=0, b=0),
+    legend=dict(y=0.5, itemsizing="constant"),
+    plot_bgcolor="#191C1F",
+)
 fig.update_traces(marker=dict(size=2))
 # Remove the logo
 fig.show(config=dict(displaylogo=False))
